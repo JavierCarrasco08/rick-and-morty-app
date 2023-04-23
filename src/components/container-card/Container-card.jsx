7a88efbb8ef15fdf15caab980b760bb651a83b3a
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-23 18:32:56 -0400

    Creando el componente contenedor de las tarjetas

---
+++ src/components/container-card/Container-card.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import Loader from "../loader/Loader";
+import Card from "../card/Card";
+
+export default function ContainerRickAndMorty({ count }) {
+  const [listCard, setListCard] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    fetch(`https://rickandmortyapi.com/api/character/?page=${count}`)
+      .then((list) => list.json())
+      .then((json) => {
+        if (!ignore) {
+          setListCard(json.results);
+        }
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, [count]);
+  return (
+    <main className="container">
+      {listCard === null ? (
+        <Loader />
+      ) : (
+        listCard.map((list) => (
+          <Card
+            key={list.id}
+            name={list.name}
+            avatar={list.image}
+            gender={list.gender}
+            species={list.species}
+            status={list.status}
+            origin={list.origin}
+          />
+        ))
+      )}
+    </main>
+  );
+}
