96ae89ce68c3963ae38c88654fe07efa61edc2c9
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-23 19:37:52 -0400

    Agregando los estilos al loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,8 +1,9 @@
 import loader from "./../../assets/loader.svg";
+import "./loader.css";
 
 export default function Loader() {
   return (
-    <figure>
+    <figure className="loader">
       <img src={loader} alt="Loader de esperar el contenido" />
     </figure>
   );
