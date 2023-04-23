a1f00d8251301d0d1ed47c2b32a0fc2e2cb794a9
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-23 19:18:11 -0400

    Agregando los botones de proximo y anterior

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import ContainerRickAndMorty from "./components/container-card/Container-card";
 import "./App.css";
+import Buttons from "./components/buttons/Buttons";
 
 function App() {
   const [count, setCount] = useState(1);
   return (
     <>
       <ContainerRickAndMorty count={count} />
+      <Buttons
+        count={count}
+        next={() => setCount(count + 1)}
+        prev={() => setCount(count - 1)}
+      />
     </>
   );
 }
---
+++ src/components/buttons/Buttons.jsx
@@ -0,0 +1,14 @@
+export default function Buttons({ count, next, prev }) {
+  return (
+    <div className="buttons">
+      {count > 1 && (
+        <button className="buttons__prev" onClick={prev}>
+          Previos
+        </button>
+      )}
+      <button className="buttons__next" onClick={next}>
+        Next
+      </button>
+    </div>
+  );
+}
